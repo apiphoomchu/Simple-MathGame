fc58dc702b9f941de9deab0004a924057be1502b
Api Phoom <apiphoom23@gmail.com>
2023-08-30 19:20:47 +0700

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Apiphoom Chuenchompoo on 30/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
