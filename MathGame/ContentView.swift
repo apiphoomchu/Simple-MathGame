c658e617287a476cb6d3e6de47eedfc2632f504f
Api Phoom <apiphoom23@gmail.com>
2023-08-30 19:27:01 +0700

    Update ContentView.swift

--- MathGame/ContentView.swift
+++ MathGame/ContentView.swift
@@ -7,13 +7,61 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var number1 = Int.random(in: 1...10)
+    @State private var number2 = Int.random(in: 1...10)
+    
+    @State private var userAnswer: String = ""
+    @State private var isCorrect: Bool? = nil
+    
+    var correctAnswer: Int {
+        return number1 * number2
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Guess the answer:")
+            Text("\(number1) × \(number2) = ?")
+                .font(.largeTitle)
+            
+            TextField("Your answer", text: $userAnswer)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
+                .padding()
+            
+            Button("Check Answer") {
+                if let userInt = Int(userAnswer), userInt == correctAnswer {
+                    isCorrect = true
+                } else {
+                    isCorrect = false
+                }
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            
+            if let correct = isCorrect {
+                if correct {
+                    Text("Correct!")
+                        .foregroundColor(.green)
+                } else {
+                    Text("Wrong! Try again.")
+                        .foregroundColor(.red)
+                }
+            }
+            
+            Button("Next") {
+                number1 = Int.random(in: 1...10)
+                number2 = Int.random(in: 1...10)
+                userAnswer = ""
+                isCorrect = nil
+            }
+            .padding()
+            .background(Color.orange)
+            .foregroundColor(.white)
+            .cornerRadius(8)
         }
         .padding()
     }
